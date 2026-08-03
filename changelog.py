ce023eb1b66975a1e0c8daad286d28940f097745
Akshat Jangid <akshatjangid16@gmail.com>
2026-08-03 12:01:16 +0530

    changelog.py get_github_repo

--- changelog.py
+++ changelog.py
@@ -1,9 +1,27 @@
 import urllib.request, json
+from urllib.parse import urlparse
+from collections import Counter
 
-pkg = "requests"
-url = f"https://pypi.org/pypi/{pkg}/json"
-data = json.load(urllib.request.urlopen(url))
+def get_github_repo(pkg):
+    url = f"https://pypi.org/pypi/{pkg}/json"
+    data = json.load(urllib.request.urlopen(url))
+    urls = data["info"]["project_urls"]
+    if not urls:
+        return None
+    all_pairs = []
+    for u in urls.values():
+        if "github.com" in u.lower():
+            parsed = urlparse(u)
+            pair = tuple(parsed.path.strip("/").split("/")[:2])
+            if pair[0] == "sponsors":
+                continue
+            all_pairs.append(pair)
+    if not all_pairs:
+        return None
+    github = Counter(all_pairs).most_common(1)[0][0]
+            
+    return github
+        
 
-print(data["info"]["version"])
-print(data["info"]["project_urls"])
-print(data["releases"])
+for p in ["requests", "pydantic", "numpy", "cowsay"]:
+    print(p, get_github_repo(p))
